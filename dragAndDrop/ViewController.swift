8f02ad9baa2a415a6275cb5cdbf6ae11551cac66
Merouane Bellaha <m.bellaha@gmail.com>
2022-01-26 21:02:31 +0100

    dragAndDrop

--- dragAndDrop/ViewController.swift
+++ dragAndDrop/ViewController.swift
@@ -9,11 +9,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mySecondView: UIView!
+
+    private let myView: UIView = {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+        view.backgroundColor = .red
+        view.isUserInteractionEnabled = true
+        return view
+    }()
+
+    private var isDragging = false
+    private var targetedView: UIView? = nil
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(myView)
     }
 
-
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        myView.center = view.center
+    }
 }
 
+// MARK: - Touch management
+
+extension ViewController {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        targetTouchedView(touch: touch, views: [myView, mySecondView])
+    }
+
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard isDragging,
+              let touch = touches.first else {
+            return
+        }
+        let location = touch.location(in: view)
+        targetedView?.center = location
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        isDragging = false
+    }
+
+    private func targetTouchedView(touch: UITouch, views: [UIView]) {
+        for view in views {
+            let location = touch.location(in: view)
+            if view.bounds.contains(location) {
+                isDragging = true
+                targetedView = view
+                return
+            }
+        }
+    }
+}
